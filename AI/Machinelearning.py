15582350bc46b4de842202c4365b61ff5676f8dc
rais youssef <raisyoussef456@gmail.com>
2024-11-20 22:41:19 +0100

    commit test 8

---
+++ AI/Machinelearning.py
@@ -0,0 +1,33 @@
+#on débute notre formation en machine learning sur une chaine youtube 'Machine learnia'
+#Machine learning = donner la machine la capacité d'apprendre sans la programmer de façon explicite 
+
+#Apprentissage supervisé :on donne des exemples à la machine pour apprendre 
+#Ces exemples on les regroupes dans un "Dataset"(tableau de données)
+
+#Deux type de problemes : 1/regression:prédire une valeur d'une variable continue (nombre infini de valeur)
+#2/classification :prédire la valeur d'une varibale discrète 
+#------------------------
+
+#Apprentissage supervisé 
+#Dataset contient :
+# "Target variable"(Y)=objectif(la variable qu'on veut la machine apprene à prédire,prix d'apartement )
+#"Features":les facteurs qui influence (target)(surface emplacement )
+#"m"est le nombre d'exemple qu'on a dans notre dataset (nbligne)
+#"n"= nb colonne (nb features) 
+
+#Modèle et ses parametres=exemple(coefficient de notre polynome )
+
+#Fonctions Cout :ensembles d'erreurs   suite à la généralisation des données
+
+# Algorithme de minimisation : algo qui minimise les erreurs  (donc minimise le cout)
+
+#---------------------------------
+# Regression linéaire 
+#voir notes téléphone   
+
+#----------------
+
+
+
+#Decent gradient =algo d'optimisation qui converge vers le minimum d'une fonction convexe 
+#permet de minimiser la fonction cout ,c'est avec ça que la machine apprend !
